4813648a92a03db7534c42b941dd0165f4ff4dd6
Sijeo Philip <sijeo80@gmail.com>
2026-03-11 21:08:32 +0530

    compile new code after tuning the parameters

--- platform/pi/.ism330dlctr_char.mod.cmd
+++
@@ -1,1 +0,0 @@
-cmd_/home/sijeophilip/dr_vehicle/platform/pi/ism330dlctr_char.mod := { echo  /home/sijeophilip/dr_vehicle/platform/pi/ism330dlctr_char.o; echo; } > /home/sijeophilip/dr_vehicle/platform/pi/ism330dlctr_char.mod
