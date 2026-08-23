6024557614d5f21bc3dafb3f2eb27c71b74c7699
Sijeo Philip <sijeo80@gmail.com>
2026-08-24 00:23:47 +0530

    modify make file in test apps

--- platform/pi/test_app/imu_simulator.d
+++
@@ -1,5 +0,0 @@
-imu_simulator.o: imu_simulator.c imu_core.h imu_wire.h
-
-imu_core.h:
-
-imu_wire.h:
--- platform/pi/test_app/test_imu_core.d
+++
@@ -1,5 +0,0 @@
-test_imu_core.o: test_imu_core.c imu_core.h imu_wire.h
-
-imu_core.h:
-
-imu_wire.h:
